9ee46756789166d2714812667d15565c3a58adf5
ManuelAlejandro20 <38479974+ManuelAlejandro20@users.noreply.github.com>
2019-10-31 18:41:03 -0300

    Actualización 31/10
    Se le agrego un par de instrucciones referentes a el análisis gramático

--- Taller1.py
+++ Taller1.py
@@ -1,35 +1,46 @@
+#Nombres: Manuel Trigo y Luciano Larama 
+from colorama import init as c_init, Fore as f
 import ply.lex as lex
 import ply.yacc as yacc
 import sys
 
+c_init(autoreset=True)
+
+variables = {}
+
 reservadas = {
         
-        'if' : 'if',
-        'print' : 'print',
-        'sum' : 'sum',
-        'begin' : 'begin',
-        'end' : 'end',
-        'matrix' : 'matrix', 
-        'scalar' : 'scalar',
-        'then' : 'then',
-        }
+    'if' : 'if',
+    'print' : 'print',
+    'sum' : 'sum',
+    'begin' : 'begin',
+    'end' : 'end',
+    'matrix' : 'matrix', 
+    'scalar' : 'scalar',
+    'then' : 'then',   
+}
 
 
 tokens = (
-        'float',
-        'char',
-        'plus',
-        'minus',
-        'mult',
-        'pow',
-        'divide',
-        'equals',
-        'lparen',
-        'lbracket',
-        'rparen',
-        'rbracket',
-        'semicolon',
-        ) + tuple(reservadas.values())
+
+    'string',
+    'float',
+    'plus',
+    'minus',
+    'mult',
+    'pow',
+    'divide',
+    'equals',
+    'lparen',
+    'lbracket',
+    'rparen',
+    'rbracket',
+    'semicolon',
+    'comment',
+
+) + tuple(reservadas.values())
+
+t_ignore = r' ' 
 
 t_plus = r'\+'
 t_minus = r'-'
@@ -42,22 +53,112 @@ t_lbracket = r'\['
 t_rparen = r'\)'
 t_rbracket = r'\]'
 t_semicolon = r'\;'
+
+def t_comment(t):
+    r'\//'
+    return
+
+def t_float(t):
+    r'\d+\.\d*(e-?\d+)?'
+    t.value = float(t.value)
+    return t
      
 def t_scalar(t):
     r'\d+'
     t.value = int(t.value)
     return t 
 
+def t_string(t):
+    r'[a-z0-9]+'
+    return t
+
+def t_matrix(t):
+    r'\[  ( \d+ (,\d+) * )   (  \;\d+ (,\d+)  *  ) * \]'
+    t.value = (t.value)
+    return t 
+
 def t_error(t):
-    print("ERROR >>> CARÁCTER NO VALIDO")
+    print(f.RED + "ERROR >>> CARÁCTER NO VALIDO")
     t.lexer.skip(1)
+    
   
 lexer = lex.lex()
 
-lexer.input('1+2^;]/=')
+#------------------------------------------------------------------------------------------#
+
+
+def p_taller(p):
+    '''
+    taller : expr
+           | asign
+
+    '''
+    
+    print(p[1])
+    for x, y in variables.items():
+        print(f.BLUE + 'Variable: ' + str(x) + f.YELLOW + ' Valor: ' + str(y))
+
+def p_asign(p):
+    '''
+    asign : string equals expr 
+    '''
+    if p[3] != '':
+        variables[p[1]] = p[3]
+        p[0] = p[3]
+        lexer.input(str(p[0]))
+        print(lexer.token().type)
+
+
+def p_op(p):
+    '''
+    expr : expr pow expr
+         | expr mult expr
+         | expr divide expr
+         | expr plus expr
+         | expr minus expr
+
+    '''
+    lexer.input(str(p[1]))
+    tipo1 = lexer.token().type
+    lexer.input(str(p[3]))
+    tipo2 = lexer.token().type
+    if(tipo1 == 'scalar' and tipo2 == 'scalar'):
+        if(p[2] == '^'):
+            p[0] = pow(p[1], p[3])
+        elif(False): pass
+        elif(False): pass
+        elif(False): pass
+        elif(False): pass
+    else:
+        p[0] = (p[2], p[1], p[3])
+
+def p_expr(p):
+    '''
+    expr : scalar
+         | matrix
+         | float
+         | string
+
+    '''
+    p[0] = p[1] 
+
+def p_error(p):
+    print(f.RED + "ERROR EN EL ANALISIS GRAMATICO")
+
+
+parser = yacc.yacc()
 
 while 1:
-    tok = lexer.token()
-    if not tok:
+    try:
+        s = input('>>> ')
+    except EOFError:
         break
-    print(tok)
+    parser.parse(s)
+
+#lexer.input('string 55de prueba55')
+
+# while 1:
+#     tok = lexer.token()
+#     if not tok:
+#         break
+#     print(tok)
